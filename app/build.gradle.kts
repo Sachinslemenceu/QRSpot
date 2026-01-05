5fe5825a5dfea0f5b7c3b3d9e9dcef13edb0e682
Sachinslemenceu <sachinslemenceu@gmail.com>
2026-01-06 00:13:38 +0530

    Added assets to the repo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     defaultConfig {
         applicationId = "com.example.qrspot"
         minSdk = 29
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
